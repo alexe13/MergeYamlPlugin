28092eee6b95e2556a49719dc2854c1bac616095
Aleksey V Golubev <aleksey.v.golubev@gmail.com>
2020-07-21 19:32:30 +0300

    initial commit

---
+++ src/main/java/ga/fundamental/merge/YamlMerger.java
@@ -0,0 +1,43 @@
+package ga.fundamental.merge;
+
+import com.amihaiemil.eoyaml.Yaml;
+import com.amihaiemil.eoyaml.YamlMapping;
+import com.amihaiemil.eoyaml.YamlPrinter;
+import com.amihaiemil.eoyaml.extensions.MergedYamlMapping;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class YamlMerger {
+
+	private final List<File> sourceFiles;
+	private final String mergedFileName;
+
+	public YamlMerger(List<File> sourceFiles, String mergedFileName) {
+		this.sourceFiles = sourceFiles;
+		this.mergedFileName = mergedFileName;
+	}
+
+	public void merge() throws IOException {
+		YamlMapping mergedMapping = sourceFiles.stream()
+				.map(this::createYamlMapping)
+				.reduce(MergedYamlMapping::new)
+				.orElseThrow(RuntimeException::new);
+		File mergedFile = new File(mergedFileName);
+		mergedFile.getParentFile().mkdirs();
+		YamlPrinter filePrinter = Yaml.createYamlPrinter(new FileWriter(mergedFile));
+
+		System.out.println(mergedMapping.toString());
+		filePrinter.print(mergedMapping);
+	}
+
+	private YamlMapping createYamlMapping(File yamlFile) {
+		try {
+			return Yaml.createYamlInput(yamlFile).readYamlMapping();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/main/java/ga/fundamental/mojo/MergeYamlMojo.java
@@ -0,0 +1,36 @@
+package ga.fundamental.mojo;
+
+import ga.fundamental.merge.YamlMerger;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Mojo(name = "merge-yaml")
+public class MergeYamlMojo extends AbstractMojo {
+
+	@Parameter(name = "finalYaml", required = true, property = "merge-yaml.finalYaml")
+	private String finalYaml;
+	@Parameter(name = "files", required = true, property = "merge-yaml.files")
+	private List<File> files;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		for (File file : files) {
+			if (!file.exists()) {
+				throw new MojoFailureException("File " + file + " does not exist");
+			}
+		}
+		//
+		try {
+			new YamlMerger(files, finalYaml).merge();
+		} catch (IOException e) {
+			throw new MojoExecutionException("Failed to merge yaml files", e);
+		}
+	}
+}
